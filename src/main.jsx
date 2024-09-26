c4c7dbf9b33b56eccb4eed1f626130fc3bbded50
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-26 08:17:08 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,14 @@
-import { StrictMode} from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import store from "./store/store.js";
 import { Provider } from "react-redux";
 
-
 createRoot(document.getElementById('root')).render(
-  
+  <StrictMode>
     <Provider store={store}>
-    <App />
-  </Provider>
-)
-
-
-/*
-export async function checkSession() { 
-  const response = await fetch('https://bookshop-backend.up.railway.app/check-session');
-  console.log("log status--->" ,response.status)
-
-  if (!response.ok) {
-    throw new Error('Logout failed');
-  }
-  return response.json();
-
-
-*/
+      <App />
+    </Provider>
+  </StrictMode>
+);
