2baeca9e439241155140274c03808c35f8e4620a
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-26 09:18:00 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./util/http.js";
-import RootPage from "./pages/RootPage.jsx";
-import ErrorPage from "./UI/ErrorPage.jsx";
-import LoginPage from "./pages/AuthenticationPages/LoginPage.jsx";
-import SignupPage from "./pages/AuthenticationPages/SignupPage.jsx";
-import PrivateRoute from "./pages/AuthenticationPages/PrivateRoute.jsx";
-import UserPage from "./pages/UserPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import DiscountPage from "./pages/DiscountPage.jsx";
-import TestimonialPage from "./pages/TestimonialPage.jsx";
-import ShopPage from "./pages/ShopPage.jsx";
-import ShopDesPage, { loader as shopLoader } from "./pages/ShopDesPage.jsx";
-import CartPage from "./pages/CartPage.jsx";
-import Wishlist from "./components/wishlist/Wishlist.jsx";
-import ErrorBoundary from "./pages/ErrorBoundary.jsx";
-import { ToastContainer } from "react-toastify";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
-import DashboardPage from "./pages/dashboard/DashboardPage.jsx";
-import Layout from "./pages/dashboard/Layout.jsx";
-import Users from "./pages/dashboard/users/Users.jsx";
-import Products from "./pages/dashboard/products/Products.jsx";
-import ErrorRoute from "./UI/ErrorRoute.jsx";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './util/http.js';
+import RootPage from './pages/RootPage.jsx';
+import ErrorPage from './UI/ErrorPage.jsx';
+import LoginPage from './pages/AuthenticationPages/LoginPage.jsx';
+import SignupPage from './pages/AuthenticationPages/SignupPage.jsx';
+import PrivateRoute from './pages/AuthenticationPages/PrivateRoute.jsx';
+import UserPage from './pages/UserPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import DiscountPage from './pages/DiscountPage.jsx';
+import TestimonialPage from './pages/TestimonialPage.jsx';
+import ShopPage from './pages/ShopPage.jsx';
+import ShopDesPage, { loader as shopLoader } from './pages/ShopDesPage.jsx';
+import CartPage from './pages/CartPage.jsx';
+import Wishlist from './components/wishlist/Wishlist.jsx';
+import ErrorBoundary from './pages/ErrorBoundary.jsx';
+import { ToastContainer } from 'react-toastify';
+import ForgotPasswordPage from './pages/ForgotPasswordPage.jsx';
+import DashboardPage from './pages/dashboard/DashboardPage.jsx';
+import Layout from './pages/dashboard/Layout.jsx';
+import Users from './pages/dashboard/users/Users.jsx';
+import Products from './pages/dashboard/products/Products.jsx';
+import ErrorRoute from './UI/ErrorRoute.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootPage />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "login", element: <LoginPage /> },
-      { path: "signup", element: <SignupPage /> },
-      { path: "discount", element: <DiscountPage /> },
-      { path: "testimonial", element: <TestimonialPage /> },
+      { path: '/', element: <HomePage /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'signup', element: <SignupPage /> },
+      { path: 'discount', element: <DiscountPage /> },
+      { path: 'testimonial', element: <TestimonialPage /> },
       {
-        path: "shop",
+        path: 'shop',
         children: [
           { index: true, element: <ShopPage /> },
-          { path: "cart", element: <CartPage /> },
-          { path: "wishlist", element: <Wishlist /> },
+          { path: 'cart', element: <CartPage /> },
+          { path: 'wishlist', element: <Wishlist /> },
           {
-            path: ":id", // Dynamic segment for shop description
-            id: "shop-details",
+            path: ':id', // Dynamic segment for shop description
+            id: 'shop-details',
             element: <ShopDesPage />,
             loader: shopLoader,
           },
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "dashboard",
+    path: 'dashboard',
     element: (
       <PrivateRoute>
         <Layout />
@@ -60,7 +60,7 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: "",
+        path: '',
         element: (
           <PrivateRoute>
             <Users />
@@ -68,7 +68,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "products",
+        path: 'products',
         element: (
           <PrivateRoute>
             <Products />
@@ -77,12 +77,12 @@ const router = createBrowserRouter([
       },
     ],
   },
-  { path: "forgot-password", element: <ForgotPasswordPage /> },
-  { path: "*", element: <ErrorRoute /> },
+  { path: 'forgot-password', element: <ForgotPasswordPage /> },
+  { path: '*', element: <ErrorRoute /> },
 ]);
 
 function App() {
-  // You can uncomment this once darkMode is needed.
+  // Uncomment if darkMode is needed
   // const darkMode = useSelector((state) => state.theme.darkMode);
   return (
     <QueryClientProvider client={queryClient}>
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import store from "./store/store.js";
-import { Provider } from "react-redux";
+import store from './store/store.js';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,3 +12,15 @@ createRoot(document.getElementById('root')).render(
     </Provider>
   </StrictMode>
 );
+
+/* Uncomment the code if needed for session checking functionality
+export async function checkSession() {
+  const response = await fetch('https://bookshop-backend.up.railway.app/check-session');
+  console.log('log status--->', response.status);
+
+  if (!response.ok) {
+    throw new Error('Logout failed');
+  }
+  return response.json();
+}
+*/
