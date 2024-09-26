50ce44aade5d3748621cd8b9a7d6b0c4f1c328ea
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-26 08:07:49 +0300

    fix react routing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
-useSelector
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./util/http.js";
 import RootPage from "./pages/RootPage.jsx";
@@ -12,7 +8,6 @@ import LoginPage from "./pages/AuthenticationPages/LoginPage.jsx";
 import SignupPage from "./pages/AuthenticationPages/SignupPage.jsx";
 import PrivateRoute from "./pages/AuthenticationPages/PrivateRoute.jsx";
 import UserPage from "./pages/UserPage.jsx";
-// import Home from './components/Home/Home';
 import HomePage from "./pages/HomePage.jsx";
 import DiscountPage from "./pages/DiscountPage.jsx";
 import TestimonialPage from "./pages/TestimonialPage.jsx";
@@ -21,16 +16,13 @@ import ShopDesPage, { loader as shopLoader } from "./pages/ShopDesPage.jsx";
 import CartPage from "./pages/CartPage.jsx";
 import Wishlist from "./components/wishlist/Wishlist.jsx";
 import ErrorBoundary from "./pages/ErrorBoundary.jsx";
-import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
- import DashboardPage from "./pages/dashboard/DashboardPage.jsx";
+import DashboardPage from "./pages/dashboard/DashboardPage.jsx";
 import Layout from "./pages/dashboard/Layout.jsx";
- 
 import Users from "./pages/dashboard/users/Users.jsx";
 import Products from "./pages/dashboard/products/Products.jsx";
 import ErrorRoute from "./UI/ErrorRoute.jsx";
- 
 
 const router = createBrowserRouter([
   {
@@ -38,64 +30,25 @@ const router = createBrowserRouter([
     element: <RootPage />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupPage />,
-      },
-      // {
-      //   path: "user", // This is the protected route
-      //   element: (
-      //     <PrivateRoute>
-      //       <UserPage />
-      //     </PrivateRoute>
-      //   ),
-      // },
-      {
-        path: "discount",
-        element: <DiscountPage />,
-      },
-      {
-        path: "testimonial",
-        element: <TestimonialPage />,
-      },
+      { path: "/", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+      { path: "discount", element: <DiscountPage /> },
+      { path: "testimonial", element: <TestimonialPage /> },
       {
         path: "shop",
-
         children: [
-          {
-            index: true, // This is the default route for "/shop"
-            element: <ShopPage />,
-          },
-          {
-            path: "cart",
-            element: <CartPage />,
-
-          },
-          {
-            path: "wishlist",
-            element: <Wishlist />,
-          },
+          { index: true, element: <ShopPage /> },
+          { path: "cart", element: <CartPage /> },
+          { path: "wishlist", element: <Wishlist /> },
           {
             path: ":id", // Dynamic segment for shop description
-            id: 'shop-details',
+            id: "shop-details",
             element: <ShopDesPage />,
             loader: shopLoader,
-
-          }
-
-        ]
+          },
+        ],
       },
-
-
-      // other routes...
     ],
   },
   {
@@ -124,42 +77,18 @@ const router = createBrowserRouter([
       },
     ],
   },
-  
-     
-  {
-    path: "forgot-password",
-    element: <ForgotPasswordPage />,
-  },
-  {
-    path: "*",
-    element: <ErrorRoute />,
-  }
-  
-  // {
-  //   path: 'discount',
-
-  //   errorElement: <ErrorPage />,
-  //   children : [
-  //     {
-  //       path: '',
-  //       element: <HomePage />
-  //     }
-  //   ]
-  // }
+  { path: "forgot-password", element: <ForgotPasswordPage /> },
+  { path: "*", element: <ErrorRoute /> },
 ]);
 
-
 function App() {
+  // You can uncomment this once darkMode is needed.
   // const darkMode = useSelector((state) => state.theme.darkMode);
-  // console.log('darkModeAppppp:', darkMode);
   return (
-    
-    // <div className={darkMode ? 'dark-theme' : ''}>
     <QueryClientProvider client={queryClient}>
       <ToastContainer />
       <RouterProvider router={router} />
     </QueryClientProvider>
-    // </div>
   );
 }
 
